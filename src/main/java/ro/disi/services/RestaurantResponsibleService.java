663df03e8cea6e1bd4e7bc26c70765143f3683c8
Tamas Pentek <pentek_pityu@yahoo.com>
2021-04-14 20:27:46 +0300

    Update restaurant responsible

--- src/main/java/ro/disi/services/RestaurantResponsibleService.java
+++ src/main/java/ro/disi/services/RestaurantResponsibleService.java
@@ -2,9 +2,13 @@ package ro.disi.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ro.disi.controllers.handlers.exceptions.model.ResourceNotFoundException;
 import ro.disi.entities.RestaurantResponsible;
 import ro.disi.repositories.AccountRepository;
 import ro.disi.repositories.RestaurantResponsibleRepository;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class RestaurantResponsibleService {
@@ -24,8 +28,24 @@ public class RestaurantResponsibleService {
             return false;
         } else {
             restaurantResponsibleRepository.save(restaurantResponsible);
-
+            System.out.println(restaurantResponsible.getId());
             return true;
         }
+    }
+
+    public UUID updateRestaurantResponsible(UUID responsibleID, RestaurantResponsible restaurantResponsible) {
+        Optional<RestaurantResponsible> optionalRestaurantResponsible = restaurantResponsibleRepository.findById(responsibleID);
+        if (!optionalRestaurantResponsible.isPresent()) {
+            throw new ResourceNotFoundException(RestaurantResponsible.class + "with id: " + responsibleID);
+        }
+        RestaurantResponsible foundRestaurantResponsible = optionalRestaurantResponsible.get();
+        foundRestaurantResponsible.setFirstName(restaurantResponsible.getFirstName());
+        foundRestaurantResponsible.setLastName(restaurantResponsible.getLastName());
+        foundRestaurantResponsible.setLocation(restaurantResponsible.getLocation());
+        foundRestaurantResponsible.setPhoneNumber(restaurantResponsible.getPhoneNumber());
+        foundRestaurantResponsible.setRestaurant(restaurantResponsible.getRestaurant());
+        foundRestaurantResponsible.setAccount(restaurantResponsible.getAccount());
+        restaurantResponsibleRepository.save(foundRestaurantResponsible);
+        return foundRestaurantResponsible.getId();
     }
 }
